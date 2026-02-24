261f36f2a434fd6940c053d5693137adb57bd884
xyca320 <easona320@gmail.com>
2026-02-24 10:21:46 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <raylib.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    InitWindow(1280, 720, "Testing v0.1");
+    SetTargetFPS(60);
+
+    int PlayerX = GetScreenWidth() / 2, PlayerY = GetScreenHeight() / 2;
+    bool IsTouchingBorder = false;
+
+    Color SKIN = { 255, 224, 189, 255 };
+
+    Texture2D Wall = LoadTexture("assets/texture/Wall.jpg");
+    vector<vector<int>> Map = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,0,0,0,0,1},
+    {1,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,1},
+    {1,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,1},
+    {1,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,1},
+    {1,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1},
+    {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1},
+    {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1},
+    {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1},
+    {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
+    while(!WindowShouldClose()) {
+
+        if (IsKeyDown(KEY_W) && !IsTouchingBorder) PlayerY -= 2;
+        if (IsKeyDown(KEY_A) && !IsTouchingBorder) PlayerX -= 2;
+        if (IsKeyDown(KEY_S) && !IsTouchingBorder) PlayerY += 2;
+        if (IsKeyDown(KEY_D) && !IsTouchingBorder) PlayerX += 2;
+
+        if (IsKeyDown(KEY_LEFT_SHIFT) && !IsTouchingBorder) {
+            if (IsKeyDown(KEY_W) && !IsTouchingBorder) PlayerY -= 5;
+            if (IsKeyDown(KEY_A) && !IsTouchingBorder) PlayerX -= 5;
+            if (IsKeyDown(KEY_S) && !IsTouchingBorder) PlayerY += 5;
+            if (IsKeyDown(KEY_D) && !IsTouchingBorder) PlayerX += 5;
+        }
+
+        BeginDrawing();
+        ClearBackground(WHITE);
+        for (int y = 0; y < Map.size(); y++) {
+            for (int x = 0; x < Map[y].size(); x++) {
+                if (Map[y][x] == 1) {
+                    DrawTexture(Wall, x * 75, y * 75, WHITE);
+                }
+            }
+        }
+        DrawCircle(PlayerX, PlayerY, 10, SKIN);
+        DrawCircle(PlayerX, PlayerY + 30, 20, SKYBLUE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
